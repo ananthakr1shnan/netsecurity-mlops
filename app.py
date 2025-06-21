8d04fd31f12ed80ec717c29524d0ed8e53a7b2a2
ananthakrishnan <ananthakrishnan073@gmail.com>
2025-06-21 14:35:43 +0530

    Updated dockerfile

--- app.py
+++ app.py
@@ -80,5 +80,5 @@ async def predict_route(request: Request,file:UploadFile=File(...)):
     
 
 if __name__ == "__main__":
-    app_run(app, host="localhost", port=8000)
-        
+    port = int(os.getenv("PORT", 8000))
+    app_run(app, host="0.0.0.0", port=port)
