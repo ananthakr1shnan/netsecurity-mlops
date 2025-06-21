441d3df4df468e76fecf5e517c93719974dd4b0a
ananthakrishnan <ananthakrishnan073@gmail.com>
2025-06-21 10:52:21 +0530

    Completed entire pipeline and tracking using mlflow

--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 @dataclass
 class DataIngestionArtifact:
-    trained_file_path: str
+    train_file_path: str
     test_file_path: str
 
 @dataclass
@@ -12,4 +12,22 @@ class DataValidationArtifact:
     valid_test_file_path: str
     invalid_train_file_path: str
     invalid_test_file_path: str
-    drigt_report_file_path: str
+    drift_report_file_path: str
+    
+@dataclass
+class DataTransformationArtifact:
+    transformed_object_file_path: str
+    transformed_train_file_path: str
+    transformed_test_file_path: str
+
+@dataclass
+class ClassificationMetricArtifact:
+    f1_score: float
+    precision_score: float
+    recall_score: float
+    
+@dataclass
+class ModelTrainerArtifact:
+    trained_model_file_path: str
+    train_metric_artifact: ClassificationMetricArtifact
+    test_metric_artifact: ClassificationMetricArtifact
